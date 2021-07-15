d8683fd1685fefd2454e9640c7e0e96847c64931
raja <Ramya@DESKTOP-C6LATET>
2021-07-15 21:21:53 +0530

    Test case updated by ramya

--- seleniumgit/src/seleniumgit/Testcase2.java
+++ seleniumgit/src/seleniumgit/Testcase2.java
@@ -6,7 +6,10 @@ public class Testcase2
 	public static void main(String[] args) 
 	{
 		System.out.println("this is a testcase2");
+		System.out.println("test case modified by ramya");
+		
 
+		
 	}
 
 }
